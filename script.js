7319ed1735a98f2cff48509e27ef2088c9a1add3
amanraina1 <amanraina9086@gmail.com>
2025-04-28 00:30:30 +0530

    web effect

---
+++ script.js
@@ -0,0 +1,266 @@
+let users = [
+  {
+    proilePic:
+      "https://plus.unsplash.com/premium_photo-1668896123988-b1566bb54579?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTMzfHxwb3RyYWl0fGVufDB8fDB8fHww",
+    displayPic:
+      "https://images.unsplash.com/photo-1524504388940-b1c1722653e1?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8ODZ8fHBvdHJhaXR8ZW58MHx8MHx8fDA%3D",
+
+    pendingMessage: 4,
+    location: "Delhi, India",
+    name: "Harshita",
+    age: 20,
+    interests: [
+      {
+        icon: "<i class='ri-music-2-fill'></i>",
+        name: "Music",
+      },
+      {
+        icon: "<i class='ri-quill-pen-fill'></i>",
+        name: "Writing",
+      },
+    ],
+    bio: "Adventure seeker. Coffee lover. Netflix binger. Dog parent. Let’s travel, laugh, and create memories together..",
+    isFriend: null,
+  },
+  {
+    proilePic:
+      "https://images.unsplash.com/photo-1635412200279-709276202012?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTI0fHxwb3RyYWl0fGVufDB8fDB8fHww",
+    displayPic:
+      "https://images.unsplash.com/photo-1680121182894-5062ef5bfe7c?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTU5fHxwb3RyYWl0fGVufDB8fDB8fHww",
+    pendingMessage: 15,
+    location: "Sydney, Austrlia",
+    name: "Avinya",
+    age: 23,
+    interests: [
+      {
+        icon: "<i class='ri-music-2-fill'></i>",
+        name: "Music",
+      },
+      {
+        icon: "<i class='ri-quill-pen-fill'></i>",
+        name: "Writing",
+      },
+    ],
+    bio: "Building dreams one coffee at a time. Lover of sunsets, music, and good conversations. Swipe right?",
+    isFriend: null,
+  },
+  {
+    proilePic:
+      "https://images.unsplash.com/photo-1643587905748-09131f983bf4?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTE2fHxwb3RyYWl0fGVufDB8fDB8fHww",
+    displayPic:
+      "https://images.unsplash.com/photo-1619024370140-d625f2e354f8?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTM0fHxwb3RyYWl0fGVufDB8fDB8fHww",
+    pendingMessage: 5,
+    location: "Phillipines, USA",
+    name: "Shivangi",
+    age: 33,
+    interests: [
+      {
+        icon: "<i class='ri-music-2-fill'></i>",
+        name: "Music",
+      },
+      {
+        icon: "<i class='ri-quill-pen-fill'></i>",
+        name: "Writing",
+      },
+    ],
+    bio: "Fluent in sarcasm, bad dance moves, and midnight snacks. Looking for my pizza-sharing partner forever.",
+    isFriend: null,
+  },
+  {
+    proilePic:
+      "https://images.unsplash.com/photo-1635412200279-709276202012?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTI0fHxwb3RyYWl0fGVufDB8fDB8fHww",
+    displayPic:
+      "https://images.unsplash.com/photo-1609505848912-b7c3b8b4beda?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTMyfHxwb3RyYWl0fGVufDB8fDB8fHww",
+    pendingMessage: 30,
+    location: "London, UK",
+    name: "Neeru",
+    age: 30,
+    interests: [
+      {
+        icon: "<i class='ri-music-2-fill'></i>",
+        name: "Music",
+      },
+      {
+        icon: "<i class='ri-quill-pen-fill'></i>",
+        name: "Writing",
+      },
+    ],
+    bio: "Gym rat with a soft side. I lift weights and occasionally heavy emotions. Let’s vibe!",
+    isFriend: null,
+  },
+  {
+    proilePic:
+      "https://images.unsplash.com/photo-1643587905748-09131f983bf4?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTE2fHxwb3RyYWl0fGVufDB8fDB8fHww",
+    displayPic:
+      "https://images.unsplash.com/photo-1608097394286-955c93b99cbb?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTYwfHxwb3RyYWl0fGVufDB8fDB8fHww",
+    pendingMessage: 10,
+    location: "Delhi, India",
+    name: "Shraddha",
+    age: 29,
+    interests: [
+      {
+        icon: "<i class='ri-music-2-fill'></i>",
+        name: "Music",
+      },
+      {
+        icon: "<i class='ri-quill-pen-fill'></i>",
+        name: "Writing",
+      },
+    ],
+    bio: "Part-time superhero, full-time foodie. Passionate about life, deep talks, and laughing until it hurts.",
+    isFriend: null,
+  },
+  {
+    proilePic:
+      "https://images.unsplash.com/photo-1635412200279-709276202012?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTI0fHxwb3RyYWl0fGVufDB8fDB8fHww",
+    displayPic:
+      "https://images.unsplash.com/photo-1611028601358-0c20ee892712?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTQ0fHxwb3RyYWl0fGVufDB8fDB8fHww",
+    pendingMessage: 1,
+    location: "Bangalore, India",
+    name: "Bhavna",
+    age: 20,
+    interests: [
+      {
+        icon: "<i class='ri-music-2-fill'></i>",
+        name: "Music",
+      },
+      {
+        icon: "<i class='ri-quill-pen-fill'></i>",
+        name: "Writing",
+      },
+      {
+        icon: "<i class='ri-book-fill'></i>",
+        name: "Reading",
+      },
+    ],
+    bio: "Bookworm. Fitness enthusiast. Amateur cook. Dreaming of road trips and spontaneous adventures with someone special.",
+    isFriend: null,
+  },
+];
+
+function select(element) {
+  return document.querySelector(element);
+}
+
+let curr = 0;
+let isAnimating = false;
+
+let deny = select(".deny");
+let accept = select(".accept");
+
+deny.addEventListener("click", () => {
+  imageChange();
+  setData(curr - 1);
+  gsap.from(".details .element", {
+    y: "100%",
+    duration: 1.5,
+    ease: Power4.easeInOut,
+    stagger: 0.06,
+  });
+});
+
+accept.addEventListener("click", () => {
+  imageChange();
+  setData(curr - 1);
+  gsap.from(".details .element", {
+    y: "100%",
+    duration: 1.5,
+    ease: Power4.easeInOut,
+    stagger: 0.06,
+  });
+});
+
+function setData(index) {
+  select(".prflimg img").src = users[index].proilePic;
+  select(".badge h5").textContent = users[index].pendingMessage;
+  select(".location h3").textContent = users[index].location;
+  select(".name h1:nth-child(1)").textContent = users[index].name;
+  select(".name h1:nth-child(2)").textContent = users[index].age;
+  let clutter = "";
+  users[index].interests.forEach((interest) => {
+    clutter += `<div class="tag flex items-center bg-white/30 px-3 py-1 rounded-full gap-1">
+    ${interest.icon}
+  <h3 class="text-sm tracking-tight">${interest.name}</h3>
+</div>`;
+  });
+  select(".tags").innerHTML = clutter;
+  select(".bio p").textContent = users[index].bio;
+}
+
+(function setInitial() {
+  select(".maincard img").src = users[curr].displayPic;
+  select(".incomingcard img").src = users[curr + 1]?.displayPic;
+
+  setData(curr);
+
+  curr = 2;
+})();
+
+function imageChange() {
+  if (!isAnimating) {
+    isAnimating = true;
+    let tl = gsap.timeline({
+      onComplete: function () {
+        isAnimating = false;
+        let main = select(".maincard");
+        let incoming = select(".incomingcard");
+
+        incoming.classList.remove("z-[2]");
+        incoming.classList.add("z-[3]");
+        incoming.classList.remove("incomingcard");
+
+        main.classList.remove("z-[3]");
+        main.classList.add("z-[2]");
+
+        gsap.set(main, {
+          scale: 1,
+          opacity: 1,
+        });
+
+        if (curr === users.length) curr = 0;
+
+        select(".maincard img").src = users[curr++].displayPic;
+
+        main.classList.remove("maincard");
+        incoming.classList.add("maincard");
+        main.classList.add("incomingcard");
+      },
+    });
+
+    tl.to(
+      ".maincard",
+      {
+        scale: 1.1,
+        opacity: 0,
+        ease: Circ,
+        duration: 0.9,
+      },
+      "start"
+    ).from(
+      ".incomingcard",
+      {
+        scale: 0.9,
+        opacity: 0,
+        ease: Circ,
+        duration: 1.1,
+      },
+      "start"
+    );
+  }
+}
+
+(function containerCreater() {
+  document.querySelectorAll(".element").forEach((element) => {
+    let div = document.createElement("div");
+    div.classList.add(`${element.classList[1]}container`, "overflow-hidden");
+    div.appendChild(element);
+    select(".details").appendChild(div);
+  });
+})();
+
+gsap.from(".details .element", {
+  y: "100%",
+  duration: 1.5,
+  ease: Power4.easeInOut,
+  stagger: 0.06,
+});
